f17cd6797d7fcc22a23cd02ec17fb73bb99f8912
Pspritechologist <naaronn@gmail.com>
2025-04-25 19:52:00 -0400

    Port args

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+	let port = std::env::args()
+		.find(|arg| arg.starts_with("--port="))
+		.and_then(|arg| arg.split_once('=').and_then(|(_, port)| port.parse::<u16>().ok()))
+		.unwrap_or_else(|| std::env::var("PORT").ok().and_then(|port| port.parse::<u16>().ok()).unwrap_or(80));
+
 	let app = Router::new()
 		.route("/", axum::routing::get(|| async { "Hello, World!" }))
 		.route("/reload", axum::routing::get(|| async { "Reloading..." }));
 
-	let listener = TcpListener::bind(("0.0.0.0", 80)).await.unwrap();
+	let listener = TcpListener::bind(("0.0.0.0", port)).await.unwrap();
 	axum::serve(listener, app).await.unwrap();
 }
