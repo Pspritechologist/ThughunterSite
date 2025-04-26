c36b101177eb3925164f45b04b7a53b152f19d26
Pspritechologist <naaronn@gmail.com>
2025-04-25 20:14:29 -0400

    Pages!

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,16 @@ async fn main() {
 		.unwrap_or_else(|| std::env::var("PORT").ok().and_then(|port| port.parse::<u16>().ok()).unwrap_or(80));
 
 	let app = Router::new()
-		.route("/", axum::routing::get(|| async { "Hello, World!" }))
-		.route("/reload", axum::routing::get(|| async { "Reloading..." }));
+		.route("/", axum::routing::get(|| async { template("we thughunting :)", "thughunters") }))
+		.route("/death", axum::routing::get(|| async { template("debit", "Sopping wet eggs served daily") }))
+		.route("/peng", axum::routing::get(|| async { "benjo" }));
 
 	let listener = TcpListener::bind(("0.0.0.0", port)).await.unwrap();
 	axum::serve(listener, app).await.unwrap();
+}
+
+fn template(title: &str, text: &str) -> String {
+	include_str!("../main_page.html")
+		.replace("[[TEXT]]", text)
+		.replace("[[TITLE]]", title)
 }
